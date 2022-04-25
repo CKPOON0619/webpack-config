fc73dc14fea99e67324fc915abc4ade468617533
CKPOON0619 <ckpoon19890129@gmail.com>
2022-04-25 12:38:33 +0100

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,13 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader:"babel-loader"
+        }  
+      }
     ],
   },
   resolve: {
